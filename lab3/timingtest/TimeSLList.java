15b1d08b9d0aac963d38b3afd3dd3d5d439d6e40
Tianqi Yang <tyang56@uic.edu>
2024-11-21 08:45:55 +0800

    lab 3

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -1,4 +1,5 @@
 package timingtest;
+
 import edu.princeton.cs.algs4.Stopwatch;
 
 /**
@@ -22,7 +23,40 @@ public class TimeSLList {
     }
 
     public static void timeGetLast() {
-        // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        int[] samples = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
+
+        timeGetLastWithSamples(Ns, times, opCounts, samples);
+
+        printTimingTable(Ns, times, opCounts);
     }
 
+    private static void timeGetLastWithSamples(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts, int[] samples) {
+        int opCount = 10000;
+
+        for (int i : samples) {
+            SLList<Integer> list = createSLList(i);
+            Ns.addLast(i);
+            times.addLast(getItems(list, opCount));
+            opCounts.addLast(opCount);
+        }
+    }
+
+    private static SLList<Integer> createSLList(int size) {
+        SLList<Integer> list = new SLList<>();
+        for (int i = 0; i < size; i++) {
+            list.addLast(i);
+        }
+        return list;
+    }
+
+    private static double getItems(SLList<Integer> list, int m) {
+        Stopwatch sw = new Stopwatch();
+        for (int i = 0; i < m; i++) {
+            list.getLast();
+        }
+        return sw.elapsedTime();
+    }
 }
